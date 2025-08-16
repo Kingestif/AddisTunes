36062a355cd57f171180cb3e505d339fd210ca1e
Kingestif <kingestiff@gmail.com>
2025-08-16 23:27:55 +0300

    feat: configure routes

--- Backend/app.js
+++ Backend/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const cors = require('cors');
+const songRouter = require('./routes/songRoutes');
 
 app.use(cors());
 app.use(morgan('dev')); 
@@ -9,5 +10,7 @@ app.use(morgan('dev'));
 app.get("/", (req, res) => {
     res.send(" Service  Is  Running");
 });
+
+app.use('/api/v1/songs', songRouter)
 
 module.exports = app;
