62a743317bce8681e874003f08bb0f34b33fe599
Kingestif <kingestiff@gmail.com>
2025-08-17 19:44:00 +0300

    refactor: refactor App component to implement routing and remove song management logic

--- Frontend/src/app.jsx
+++ Frontend/src/app.jsx
@@ -1,57 +1,21 @@
-import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { 
-  fetchSongs, addSong, updateSong, deleteSong 
-} from "./redux/songsSlice";
 import SongList from "./components/SongList";
 import { Header } from "./components/Header";
 import { HeroSection } from "./components/HeroSection";
 import { Footer } from "./components/Footer";
+import {SongsSection} from "./components/SongsSection";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React from "react";
 
 const App = () => {
-  const dispatch = useDispatch();
-  const { items, loading } = useSelector((state) => state.songs);
-
-  useEffect(() => {
-    dispatch(fetchSongs());
-  }, [dispatch]);
-
-  // Add a new song
-  const handleAdd = () => {
-    const title = prompt("Enter song title:");
-    if (title) dispatch(addSong({ title }));
-  };
-
-  // Update an existing song
-  const handleUpdate = (song) => {
-    const newTitle = prompt("Edit song title:", song.title);
-    if (newTitle) dispatch(updateSong({ id: song._id, data: { ...song, title: newTitle } }));
-  };
-
-  // Delete a song
-  const handleDelete = (_id) => {
-    if (window.confirm("Are you sure you want to delete this song?")) {
-      dispatch(deleteSong(_id));
-    }
-  };
-
   return (
-    <div>
-      <Header/>
-      <HeroSection/>
-      {/* <h1>Songs</h1>
-      <button onClick={handleAdd}>Add Song</button>
-      {loading ? (
-        <p>Loading...</p>
-      ) : (
-        <SongList 
-          songs={items} 
-          onEdit={handleUpdate} 
-          onDelete={handleDelete} 
-        />
-      )} */}
-      <Footer/>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<HeroSection />} />
+        <Route path="/songs" element={<SongsSection />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   );
 };
 
