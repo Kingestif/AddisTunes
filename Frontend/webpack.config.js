cf7e99137b0f9287376d68481f0a03c3fa9aa6dc
Kingestif <kingestiff@gmail.com>
2025-08-17 10:06:12 +0300

    feat: add webpack configuration for development environment

---
+++ Frontend/webpack.config.js
@@ -0,0 +1,49 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
+require("dotenv").config();
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.[contenthash].js",
+    clean: true,
+    publicPath: "/",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: "babel-loader",
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|svg)$/i,
+        type: "asset/resource", // custom rule for images
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+    new webpack.DefinePlugin({
+      "process.env.API_BASE_URL": JSON.stringify(process.env.API_BASE_URL),
+    }),
+  ],
+  devServer: {
+    historyApiFallback: true,
+    port: 3000,
+    open: true,
+    hot: true,
+  },
+  mode: "development",
+};
