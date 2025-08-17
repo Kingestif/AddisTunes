484da11c09a78fc37b822f9cdf9b4cb06ebf915a
Kingestif <kingestiff@gmail.com>
2025-08-17 13:01:24 +0300

    feat: implement CRUD operations for songs in Redux saga and slice

--- Frontend/src/redux/songsSaga.js
+++ Frontend/src/redux/songsSaga.js
@@ -1,11 +1,49 @@
 import { call, put, takeLatest } from "redux-saga/effects";
 import axios from "axios";
-import { fetchSongs, fetchSongsSuccess, fetchSongsFailure } from "./songsSlice";
+import {
+  fetchSongs, fetchSongsSuccess, fetchSongsFailure,
+  addSong, addSongSuccess,
+  updateSong, updateSongSuccess,
+  deleteSong, deleteSongSuccess
+} from "./songsSlice";
 
+const API = process.env.API_BASE_URL;
+
+// FETCH
 function* fetchSongsSaga() {
   try {
-    const res = yield call(axios.get, `${process.env.API_BASE_URL}`);
+    const res = yield call(axios.get, `${API}/?page=${2}&limit=${10}`);
     yield put(fetchSongsSuccess(res.data.data.songs));
+  } catch (error) {
+    yield put(fetchSongsFailure(error.message));
+  }
+}
+
+// ADD
+function* addSongSaga(action) {
+  try {
+    const res = yield call(axios.post, `${API}`, action.payload);
+    yield put(addSongSuccess(res.data.data.song));
+  } catch (error) {
+    yield put(fetchSongsFailure(error.message));
+  }
+}
+
+// UPDATE
+function* updateSongSaga(action) {
+  try {
+    const res = yield call(axios.patch, `${API}/${action.payload.id}`, action.payload.data);
+    yield put(updateSongSuccess(res.data.data.song));
+  } catch (error) {
+    yield put(fetchSongsFailure(error.message));
+  }
+}
+
+// DELETE
+function* deleteSongSaga(action) {
+  try {
+    yield call(axios.delete, `${API}/${action.payload}`);
+    yield put(deleteSongSuccess(action.payload));
   } catch (error) {
     yield put(fetchSongsFailure(error.message));
   }
@@ -13,4 +51,7 @@ function* fetchSongsSaga() {
 
 export default function* songsSaga() {
   yield takeLatest(fetchSongs.type, fetchSongsSaga);
+  yield takeLatest(addSong.type, addSongSaga);
+  yield takeLatest(updateSong.type, updateSongSaga);
+  yield takeLatest(deleteSong.type, deleteSongSaga);
 }
--- Frontend/src/redux/songsSlice.js
+++ Frontend/src/redux/songsSlice.js
@@ -9,28 +9,33 @@ const songsSlice = createSlice({
     page: 1,
   },
   reducers: {
-    fetchSongs: (state, action) => { state.loading = true; },
-    fetchSongsSuccess: (state, action) => {
-      state.items = action.payload;
-      state.loading = false;
-    },
-    fetchSongsFailure: (state, action) => {
-      state.error = action.payload;
-      state.loading = false;
-    },
+    fetchSongs: (state) => { state.loading = true; },
+    fetchSongsSuccess: (state, action) => { state.items = action.payload; state.loading = false; },
+    fetchSongsFailure: (state, action) => { state.error = action.payload; state.loading = false; },
+
     addSong: (state) => { state.loading = true; },
+    addSongSuccess: (state, action) => { state.items.push(action.payload); state.loading = false; },
+
     updateSong: (state) => { state.loading = true; },
+    updateSongSuccess: (state, action) => {
+      const index = state.items.findIndex(s => s._id === action.payload._id);
+      if (index !== -1) state.items[index] = action.payload;
+      state.loading = false;
+    },
+
     deleteSong: (state) => { state.loading = true; },
+    deleteSongSuccess: (state, action) => {
+      state.items = state.items.filter(s => s._id !== action.payload);
+      state.loading = false;
+    },
   },
 });
 
 export const {
-  fetchSongs,
-  fetchSongsSuccess,
-  fetchSongsFailure,
-  addSong,
-  updateSong,
-  deleteSong,
+  fetchSongs, fetchSongsSuccess, fetchSongsFailure,
+  addSong, addSongSuccess,
+  updateSong, updateSongSuccess,
+  deleteSong, deleteSongSuccess
 } = songsSlice.actions;
 
 export default songsSlice.reducer;
