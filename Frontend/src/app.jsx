6db8507ff1356f9ade5b8cdefc6b45e80aeeb9ad
Kingestif <kingestiff@gmail.com>
2025-08-17 13:01:52 +0300

    feat: add song management functions (add, update, delete) in App component

--- Frontend/src/app.jsx
+++ Frontend/src/app.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { fetchSongs } from "./redux/songsSlice";
+import { 
+  fetchSongs, addSong, updateSong, deleteSong 
+} from "./redux/songsSlice";
 import SongList from "./components/SongList";
 
 const App = () => {
@@ -11,10 +13,38 @@ const App = () => {
     dispatch(fetchSongs());
   }, [dispatch]);
 
+  // Add a new song
+  const handleAdd = () => {
+    const title = prompt("Enter song title:");
+    if (title) dispatch(addSong({ title }));
+  };
+
+  // Update an existing song
+  const handleUpdate = (song) => {
+    const newTitle = prompt("Edit song title:", song.title);
+    if (newTitle) dispatch(updateSong({ id: song._id, data: { ...song, title: newTitle } }));
+  };
+
+  // Delete a song
+  const handleDelete = (_id) => {
+    if (window.confirm("Are you sure you want to delete this song?")) {
+      dispatch(deleteSong(_id));
+    }
+  };
+
   return (
     <div>
       <h1>Songs</h1>
-      {loading ? <p>Loading...</p> : <SongList songs={items} />}
+      <button onClick={handleAdd}>Add Song</button>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <SongList 
+          songs={items} 
+          onEdit={handleUpdate} 
+          onDelete={handleDelete} 
+        />
+      )}
     </div>
   );
 };
