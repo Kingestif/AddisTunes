bfa2a762877d51c4984eb4cea047851b52fe2eff
Kingestif <kingestiff@gmail.com>
2025-08-18 08:58:20 +0300

    feat:  configure webpack for development mode

--- Frontend/webpack.config.js
+++ Frontend/webpack.config.js
@@ -3,28 +3,31 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const webpack = require("webpack");
 require("dotenv").config();
 
+const mode = process.env.NODE_ENV || "development";
+
 module.exports = {
+  mode,
   entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.[contenthash].js",
     clean: true,
-    publicPath: "/",
+    publicPath: "/", 
   },
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/,
+        test: /\.(js|jsx)$/,  
         exclude: /node_modules/,
         use: "babel-loader",
       },
       {
-        test: /\.css$/,
+        test: /\.css$/,        
         use: ["style-loader", "css-loader"],
       },
       {
         test: /\.(png|jpg|jpeg|gif|svg)$/i,
-        type: "asset/resource", // custom rule for images
+        type: "asset/resource",
       },
     ],
   },
@@ -33,7 +36,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./public/index.html",
+      template: "./public/index.html", 
     }),
     new webpack.DefinePlugin({
       "process.env.API_BASE_URL": JSON.stringify(process.env.API_BASE_URL),
@@ -45,5 +48,4 @@ module.exports = {
     open: true,
     hot: true,
   },
-  mode: "development",
 };
